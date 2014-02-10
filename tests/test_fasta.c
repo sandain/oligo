a6534b27dd6499dce76d4c981c13b14de2f1bbad
Jason M. Wood <sandain@hotmail.com>
2014-02-09 22:34:04 -0700

    add more tests to test_fasta

--- tests/test_fasta.c
+++ tests/test_fasta.c
@@ -3,25 +3,54 @@
 #include "../src/fasta.h"
 #include "../src/sequence.h"
 
+Fasta * fasta;
 
-START_TEST (size) {
-  Fasta * fasta = newFasta ("test_fasta.nwk");
-  ck_assert_msg (
-    fasta->size == 2,
-    "Expected a fasta size of 2.\n"
+void setup (void) {
+  fasta = newFasta ("test_fasta.nwk");
+}
+
+void teardown (void) {
+  freeFasta (fasta);
+}
+
+START_TEST (test_fasta_size) {
+  ck_assert_int_eq (
+    fasta->size,
+    2
   );
 } END_TEST
 
+START_TEST (test_fasta_length) {
+  ck_assert_int_eq (
+    fasta->lengths[0],
+    2268
+  );
+  ck_assert_int_eq (
+    fasta->lengths[1],
+    2268
+  );
+} END_TEST
 
+START_TEST (test_fasta_identifiers) {
+  ck_assert_str_eq (
+    fasta->ids[0],
+    "gb|CP000239.1|:2536947-2539214"
+  );
+  ck_assert_str_eq (
+    fasta->ids[1],
+    "gi|86607503:c28351-26084"
+  );
+} END_TEST
 
 Suite * fasta_suite (void) {
   Suite *s = suite_create ("Fasta");
 
   /* Core test case */
   TCase *tc_core = tcase_create ("Core");
-  tcase_add_test (tc_core, size);
-
-
+  tcase_add_checked_fixture (tc_core, setup, teardown);
+  tcase_add_test (tc_core, test_fasta_size);
+  tcase_add_test (tc_core, test_fasta_length);
+  tcase_add_test (tc_core, test_fasta_identifiers);
 
   suite_add_tcase (s, tc_core);
 
